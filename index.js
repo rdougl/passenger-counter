109833a6bbe860fc4275c576d992b67812ac6978
rdougl <117421869+rdougl@users.noreply.github.com>
2022-12-13 18:18:36 -0500

    Added old files

--- index.js
+++ index.js
@@ -1,15 +1,51 @@
-let saveEl = document.getElementById("save-el")
-let countEl = document.getElementById("count-el")
+// document.getElementById("count").innerText = 5
+
+let countEl = document.getElementById("count-el") // pass in argument
 let count = 0
 
 function increment() {
     count += 1
-    countEl.textContent = count
+    countEl.innerText = count
+    //document.getElementById("pep-el").innerText = "Here are the number of People Entered"
+    console.log(count);
 }
+let saveEl = document.getElementById("save-el") // pass in argument
+let countSave = "None"
+//let countOut = "None"
+
 
 function save() {
-    let countStr = count + " - "
-    saveEl.textContent += countStr
-    countEl.textContent = 0
+
+    let countSave = count + " - "
+    saveEl.textContent += countSave
+    countEl.innerText = 0
     count = 0
+    // if (countOut = "") {
+    //     console.log(countOut);
+    //     countSave =  count 
+    // countOut =  countSave 
+    // console.log(countSave);
+    // console.log(countOut);
+    // count = 0
+    // countEl.innerText = count
+    // }
+    
+    // else {
+    //     console.log(countSave);
+    //     console.log(countOut);
+    //     console.log(count);
+    //     countSave =  count 
+    // countOut = countOut + "-m" + countSave 
+    // console.log(countOut);
+    // console.log(countSave);
+    // count = 0
+    // countEl.innerText = count
+    // }
+    
 }
+
+
+ console.log(count);
+
+
+
